6ea62a86b130eb48a33f5a25e16ac3acd971a116
Douglas Oliveira <109635136+Djgoliveira@users.noreply.github.com>
2022-12-15 03:19:27 -0300

    Update alunos.js
    atualização

--- alunos.js
+++ alunos.js
@@ -1,61 +1,56 @@
-let aluno = {
-  nome:"",
-  totalFaltas: 0,
-  notas: [0],
-  mediaNotas: function(listaAlunos) {
-      var arr= listaAlunos.notas;
-      var divisorMedia = arr.length;
-      var Media = arr.reduce(function(calcularMedia,i){
-      return calcularMedia+i;        
-});
-return(`As notas do(a) aluno(a) ${listaAlunos.nome} são: ${listaAlunos.notas} com isso sua média é:`, Media/divisorMedia);
-    
-
-  },
-  aumentarFaltas: function(listaAlunos) {
-      var falta = listaAlunos.faltas;
-      var aumentarFaltas = 1;
-      var somarFaltas = (falta + aumentarFaltas);  
-       return (`Aluno(a) ${listaAlunos.nome} tinha ${falta} faltas, recebeu mais ${aumentarFaltas} e com isso agora tem: ${somarFaltas} faltas`);
-  },
-}
 /*
-function Aluno(nome, faltas, notas) {
-    this.nome = nome;
-    this.faltas = faltas;
-    this.notas = notas;
-  }
-  */
+Micro desafio - Passo 1
+Crie um objeto aluno que tenha como atributos: nome (string), quantidade de faltas
+(number) e notas (array de números). Crie um construtor para ele e importe-o como o
+módulo aluno.
+*/
 
-  class alunos {
-    constructor (nome, faltas , notas){
-        this.nome = nome;
-        this.faltas = faltas;
-        this.notas = notas;
-    }
-}
+let aluno ={
+    nome:"",
+    totalFaltas: 0,
+    notas: [0],
+    mediaNotas: function(listaAlunos) {
+        var arr= listaAlunos.notas;
+        var divisorMedia = arr.length;
+        var Media = arr.reduce(function(calcularMedia,i){
+        return calcularMedia+i;        
+  });
+  return(`As notas do(a) aluno(a) ${listaAlunos.nome} são: ${listaAlunos.notas} com isso sua média é:`, Media/divisorMedia);
+      
   
-const aluno1 = new alunos('Gabriel',2, [3,7,7,1,6]);
-const aluno2 = new alunos('Pietro',1, [3,5,7,8,9]);
-const aluno3 = new alunos('Carol', 2, [8,5,1,10,3]);
-const aluno4 = new alunos("Tamires", 3, [4,8,9,3,10]);
+    },
+    aumentarFaltas: function(listaAlunos) {
+        var falta= listaAlunos.totalFaltas;
+        var aumentarFaltas= 1;
+        var somarFaltas = (falta + aumentarFaltas);  
+         return (`Aluno(a) ${listaAlunos.nome} tinha ${falta} faltas, recebeu mais ${aumentarFaltas} e com isso agora tem: ${somarFaltas} faltas`);
+    },
+}
 
+  class ModuloAluno {
+    constructor(nome, totalFaltas, notas){
+        this.nome = nome;
+        this.totalFaltas = totalFaltas;
+        this.notas = notas; 
+             
+    } 
+    
+  }
 
-
+const aluno1 = new ModuloAluno('Gabriel',2, [3,7,7,1,6]);
+const aluno2 = new ModuloAluno('Pietro',1, [3,5,7,8,9]);
+const aluno3 = new ModuloAluno('Carol', 2, [8,5,1,10,3]);
+const aluno4 = new ModuloAluno("Tamires", 3, [4,8,9,3,10]);
 
 let listaAlunos = [
-  aluno1,
-  aluno2,
-  aluno3,
-  aluno4
-  
-
+    aluno1,
+    aluno2,
+    aluno3,
+    aluno4 
 ];
-//console.log (aluno4.notas);
-//console.log(aluno.mediaNotas(aluno4));
-//console.log(aluno.aumentarFaltas(aluno4));
 
-module.exports = { alunos, listaAlunos};
-//module.exports = {listaAlunos, alunos};
+console.log (listaAlunos);
+console.log(aluno.mediaNotas(aluno1));
+console.log(aluno.aumentarFaltas(aluno4));
 
-
+  module.exports = {ModuloAluno,listaAlunos, aluno}; 
